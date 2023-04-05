9d2c2532c433540f3ce4a18f61391bbeeca64693
Douglas Galdino <dougg0k@gmail.com>
2023-04-05 10:48:41 -0300

    add rawaccel script

---
+++ scripts/apps/rawaccel_profiles.ps1
@@ -0,0 +1,39 @@
+Push-Location $PSScriptRoot
+
+$title = "RawAccel Profiles"
+$prompt = "Select a RAW Accel Profile"
+$profiles = @(Get-ChildItem -Path .\ -Filter *.json -r | % { $_.Name })
+
+function Apply-Profile {
+	[CmdletBinding()]
+	param(
+		[Parameter(Mandatory)]
+		[string]$profile_name
+	)
+	$value = $profiles[$profile_name]
+	$exists = $profiles.Contains($value)
+	if ($exists) {
+		.\writer.exe $value
+		Write-Host "Profile $value applied successfully!"
+		break
+	} else {
+		Write-Host "Profile invalid, try again!"
+	}
+}
+
+Write-Host $title
+[Environment]::NewLine
+
+For ($i=0; $i -lt $profiles.Length; $i++) {
+    $label = $profiles[$i]
+    Write-Host "[$i] $label"
+}
+
+while ($true) {
+	[Environment]::NewLine
+	[int]$result = Read-Host $prompt
+	Apply-Profile -profile_name $result
+}
+
+[Environment]::NewLine
+cmd /c pause
