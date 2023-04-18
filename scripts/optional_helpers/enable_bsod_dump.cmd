3d12a15456dbc26c08044b07883283b3c0eb32dd
Douglas Galdino <dougg0k@gmail.com>
2023-04-18 10:26:25 -0300

    Update enable_bsod_dump.cmd

--- scripts/optional_helpers/enable_bsod_dump.cmd
+++ scripts/optional_helpers/enable_bsod_dump.cmd
@@ -1,4 +1,8 @@
 :: To help identify causes of BSOD
 :: Use WinDBG, download the SDK and install only Debugging Tools for Windows -  https://learn.microsoft.com/en-us/windows-hardware/drivers/debugger/debugger-download-tools
 :: After having enabled and BSOD have happend, you should find the dump at %SystemRoot%\Minidump or %SystemRoot%\MEMORY.DMP
-wmic RECOVEROS set DebugInfoType = 1
+:: Alternatively, visualize through this tool - https://www.nirsoft.net/utils/blue_screen_view.html
+
+wmic recoveros set DebugInfoType = 3
+wmic recoveros set AutoReboot = True
+wmic recoveros set WriteToSystemLog = True
