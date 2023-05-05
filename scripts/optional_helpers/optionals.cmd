76bf337242d497cdcf930dc8256f72428fffcb4a
Douglas Galdino <dougg0k@gmail.com>
2023-05-05 19:50:42 -0300

    added optionals script

---
+++ scripts/optional_helpers/optionals.cmd
@@ -0,0 +1,3 @@
+REG ADD "HKEY_CLASSES_ROOT\*\shell\Open with VS Codium" /v @ /t REG_SZ /d "Edit with VS Codium" /f
+REG ADD "HKEY_CLASSES_ROOT\*\shell\Open with VS Codium" /v Icon /t REG_SZ /d "C:\\Users\\PC\\AppData\\Local\\Programs\\VSCodium\\VSCodium.exe" /f
+REG ADD "HKEY_CLASSES_ROOT\*\shell\Open with VS Codium\command" /v @ /t REG_SZ /d "\"C:\\Users\\PC\\AppData\\Local\\Programs\\VSCodium\\VSCodium.exe\" \"%1\"" /f
