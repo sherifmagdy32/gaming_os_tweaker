1ab8b04e381a6fcaaa7e914b0d3b974c2e32dd96
Douglas Galdino <dougg0k@gmail.com>
2023-05-20 01:21:51 -0300

    updated script

--- scripts/debloat/cleanup.cmd
+++ scripts/debloat/cleanup.cmd
@@ -8,3 +8,9 @@ for /d %%x in ("%temp%\NVIDIA Corporation\NV_Cache\*") do @rd /s /q "%%x"
 :: Fix Windows folder loading problem
 REG DELETE "HKEY_CURRENT_USER\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\BagMRU" /f
 REG DELETE "HKEY_CURRENT_USER\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\Bags" /f
+
+:: Windows temp folders cleanup
+del /s /f /q %userprofile%\Recent\*.*
+del /s /f /q %systemdrive%\Windows\Prefetch\*.*
+del /s /f /q %systemdrive%\Windows\Temp\*.*
+del /s /f /q %USERPROFILE%\appdata\local\temp\*.*
