d2079bcd029a37301616a2fb0db1c6a09bd630ff
Douglas Galdino <dougg0k@gmail.com>
2023-05-26 20:10:26 -0300

    updated notes of new script

--- scripts/optional_helpers/interrupt_affinity_auto.cmd
+++ scripts/optional_helpers/interrupt_affinity_auto.cmd
@@ -15,17 +15,19 @@
 :: It could automate based on CPU Cores amount
 :: Filter by GPU, USB Mouse and LAN
 :: Apply same core to each + parent device
-:: Just apply tweaks if more 4 or more cores 
+:: Just apply tweaks if more 4 or more cores
 :: As long as not on Core0 should work
 
 :: Besides that, I could set DevicePriority to High in both Mouse and LAN.
 :: Also undo MSI from Mouse.
 :: Add MSI to all other devices.
 :: Reset DevicePriority from all other devices
+:: It would make use of regedit to alter certain aspects
 
-:: There could be certain user choices depending how much results can be filtered.
+:: There could be certain user choices depending how much results can be filtered. Also in some cases, there are multiple of the same devices, and it could have a different device parent, hence, each child device would need to be dealt separately. But I could use one core per each type of device. If multiple mouses, all in same core, along with its parent.
 
-:: This would replace Interrupt Affinity Tool and MSI Tool
+:: This would replace Interrupt Affinity Tool and MSI Tool.
+:: It would be based on pre-choices in attempt to reduce latency to the max in every aspect. Like some mentioned above.
 
 :: https://learn.microsoft.com/en-us/powershell/module/pnpdevice/get-pnpdeviceproperty
 :: https://learn.microsoft.com/en-us/powershell/module/pnpdevice/get-pnpdevice
