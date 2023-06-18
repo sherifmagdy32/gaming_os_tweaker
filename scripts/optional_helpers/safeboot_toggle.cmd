4f90c2958e9f6ca9148cb3070e02dbf4fcee5d05
Douglas Galdino <dougg0k@gmail.com>
2023-06-18 02:36:32 -0300

    add comment to safeboot script

--- scripts/optional_helpers/safeboot_toggle.cmd
+++ scripts/optional_helpers/safeboot_toggle.cmd
@@ -10,6 +10,7 @@ if [%property%]==[] (
 	bcdedit /deletevalue {default} safeboot
 )
 
+:: Beware, if you have set to enter SafeMode in the next boot, after you enter safemode, you need to execute this script again, to remove the entry, otherwise you will enter safemode again in a next reboot.
 
 :: To check manual if the value is set.
 :: bcdedit /enum
